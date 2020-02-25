e8168bf1b57d82e7ad2bdd6a19eca945cb5f069c
Mohamed Akram Khaleelur Rahman <akrammohamed123@outlook.com>
2020-02-25 07:55:57 +0000

    change

---
+++ CloudWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CloudWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
